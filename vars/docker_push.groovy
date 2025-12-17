f72bdf8f93bdc02fe742320df94488dc5dd3a6d0
Chethankumrc <103442595+Chethankumrc@users.noreply.github.com>
2025-12-17 19:31:08 +0530

    Update docker_push.groovy
    updated library script

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,10 @@
 def call(String Project, String ImageTag, String dockerHubUser){
-  sh "withCredentials([usernamePassword(credentailsId:"dockerHubCred", passwordVariable: "dockerHubPass", usernameVariable: "dockerHubuser"){
+  withcredentials([
+    usernamePassword(
+      credentailsId:"dockerHubCred",
+      passwordVariable: "dockerHubPass",
+      usernameVariable: "dockerHubuser"
+    ){
   sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
   sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
 }
